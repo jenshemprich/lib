72eacdbcbfe04174123d40e65b5c5471f29939ba
Jens <jens@github.com>
2019-01-10 18:49:24 +0100

    Added /fp:fast and /GL configuration for benchmarking

--- build/build_tensorflow_with_cmake.bat
+++ build/build_tensorflow_with_cmake.bat
@@ -43,17 +43,38 @@ REM /O2 is automatically set by cmake
 if /I %VS_SOLUTION_DIR_NAME%==Debug (
     set TENSORFLOW_WIN_CPU_SIMD_OPTION="/arch:AVX"
     set CMAKE_CONFIGURATION_TYPE=Debug
+        set CMAKE_CXX_FLAGS=
+        set CMAKE_LINKER_FLAGS=
 ) else (
     if /I not x%VS_SOLUTION_DIR_NAME:avx2=%==x%VS_SOLUTION_DIR_NAME% (
         set TENSORFLOW_WIN_CPU_SIMD_OPTION="/arch:AVX2"
+
+        REM global optimization build for benchmarking (does not work yet)
+        if /I not x%VS_SOLUTION_DIR_NAME:avx2-fma-gl=%==x%VS_SOLUTION_DIR_NAME% (
+            echo "Warning: using option /GL exceeds COFF file size of 4G"
+            echo "Warning:  cmake doesn't set /LTCG not set for modules"
+            echo "cmake doesn't set ""Whole Program Optimization"" in project general section either"
+            set CMAKE_CXX_FLAGS="/fp:fast /GL"
+            set CMAKE_LINKER_FLAGS="/LTCG"
+        ) else (
+            REM explicit /fp:fast build for benchmarking
+            if /I not x%VS_SOLUTION_DIR_NAME:avx2-fma=%==x%VS_SOLUTION_DIR_NAME% (
+                set CMAKE_CXX_FLAGS="/fp:fast"
+                set CMAKE_LINKER_FLAGS=
+            ) else (
+                set CMAKE_CXX_FLAGS=
+                set CMAKE_LINKER_FLAGS=
+            )
+        )
+
         set CMAKE_CONFIGURATION_TYPE=Release
     ) else (
         set TENSORFLOW_WIN_CPU_SIMD_OPTION="/arch:AVX"
         set CMAKE_CONFIGURATION_TYPE=Release
+        set CMAKE_CXX_FLAGS=
+        set CMAKE_LINKER_FLAGS=
     )
 )
-
-REM TODO global optimization with /GL -> set separately from /arch since cmake can only detect a single flag
 
 if /I not x%VS_SOLUTION_DIR_NAME:cuda=%==x%VS_SOLUTION_DIR_NAME% (
     set TENSORFLOW_ENABLE_GPU_FLAG="ON"
@@ -72,6 +93,10 @@ pushd %TENSORFLOW_SOURCES%\tensorflow\contrib\cmake
 cmake . -B%CMAKE_TENSORFLOW_BUILD_PATH% -A x64 ^
 -G %CMAKE_GENERATOR% ^
 -DCMAKE_CONFIGURATION_TYPES=%CMAKE_CONFIGURATION_TYPE% ^
+-DCMAKE_CXX_FLAGS=%CMAKE_CXX_FLAGS% ^
+-DCMAKE_EXE_LINKER_FLAGS=%CMAKE_LINKER_FLAGS% ^
+-DCMAKE_SHARED_LINKER_FLAGS=%CMAKE_LINKER_FLAGS% ^
+-DCMAKE_MODULE_LINKER_FLAGS=%CMAKE_LINKER_FLAGS% ^
 -Dtensorflow_BUILD_ALL_KERNELS=ON ^
 -Dtensorflow_BUILD_CC_EXAMPLE=ON ^
 -Dtensorflow_BUILD_CC_TESTS=%TENSORFLOW_BUILD_CC_TESTS_FLAG% ^
