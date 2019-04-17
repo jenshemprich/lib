e82319e4226cf62d255d75339d6e7f4ff46a546b
Jens <jens@github.com>
2019-04-17 21:42:28 +0200

    Splitted main build script into cpu/gpu configurations

--- build/build_tensorflow_r1.10-cmake-cpu.bat
+++ build/build_tensorflow_r1.10-cmake-cpu.bat
@@ -7,11 +7,5 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 call build_tensorflow_with_cmake.bat Release-avx2
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-call build_tensorflow_with_cmake.bat Release-cuda
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-call build_tensorflow_with_cmake.bat Release-cuda-avx2
-if %errorlevel% neq 0 exit /b %errorlevel%
-
 call build_tensorflow_with_cmake.bat Release-avx2-fma
 if %errorlevel% neq 0 exit /b %errorlevel%
